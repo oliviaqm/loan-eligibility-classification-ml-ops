f7dc8b1a4cf3b0ffe898950c89522c8ad5d1dc37
Hari Vigneshwaran S <hari-12180@hari-12180.csez.zohocorpin.com>
2021-10-10 17:44:56 +0530

    Port update

--- app.py
+++ app.py
@@ -125,5 +125,4 @@ def eligibility_check():
 
 
 if __name__ == "__main__":
-     app.run( debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)) )
-         label="Regression_model", linewidth=2) 
+     app.run( debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080))) 
